1bb11ab554b35a4b3dde36cffd323d0fe6750ddd
Sai Himal Allu <saihimallu1010@gmail.com>
2017-09-28 03:00:15 +0530

    Update control_unit.v
    Updated the control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -1,5 +1,23 @@
 `timescale 1ns / 1ps
-
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/28/2017 02:54:41 AM
+// Design Name: 
+// Module Name: control_unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 module control unit {
   instruction,
